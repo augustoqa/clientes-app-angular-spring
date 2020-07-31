51c305af71ce5d63986281c7824b3a34c5f9a8c6
Cesar Acual <augustoacual@gmail.com>
2020-07-31 11:10:56 -0600

    Escribiendo implementacion crear en el cliente service y en FormComponent

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,20 +1,23 @@
-import { Injectable } from '@angular/core';
-import { Cliente } from './cliente.js';
-import { Observable, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators'
+import { Injectable } from "@angular/core";
+import { Cliente } from "./cliente.js";
+import { Observable, of } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable()
 export class ClienteService {
-
   private urlEndPoint: string = "http://localhost:8080/api/clientes";
 
-  constructor(private http: HttpClient) { }
+  private httpHeaders = new HttpHeaders({ "Content-Type": "application/json" });
+
+  constructor(private http: HttpClient) {}
 
   getClientes(): Observable<Cliente[]> {
     return this.http.get<Cliente[]>(this.urlEndPoint);
-    // return this.http.get(this.urlEndPoint).pipe(
-    //   map(response => response as Cliente[])
-    // );
+  }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {
+      headers: this.httpHeaders,
+    });
   }
 }
