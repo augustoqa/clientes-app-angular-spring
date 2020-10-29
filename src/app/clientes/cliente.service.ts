7d413d169cd4b08d309a8fe99d00efe30402e99d
Cesar Acual <augustoacual@gmail.com>
2020-10-29 16:17:47 -0600

    Manejo de error en el Frontend Angular en obtener por id

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from "@angular/core";
 import { Cliente } from "./cliente.js";
-import { Observable, of } from "rxjs";
+import { Observable, of, throwError } from "rxjs";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { catchError } from "rxjs/operators";
+import Swal from "sweetalert2";
+
+import { Router } from "@angular/router";
 
 @Injectable()
 export class ClienteService {
@@ -9,7 +13,8 @@ export class ClienteService {
 
   private httpHeaders = new HttpHeaders({ "Content-Type": "application/json" });
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+              private router: Router) {}
 
   getClientes(): Observable<Cliente[]> {
     return this.http.get<Cliente[]>(this.urlEndPoint);
@@ -22,7 +27,14 @@ export class ClienteService {
   }
 
   getCliente(id): Observable<Cliente> {
-    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`);
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
+      catchError(e => {
+        this.router.navigate(['/clientes'])
+        console.error(e.error.mensaje);
+        Swal.fire('Error al editar', e.error.mensaje, 'error');
+        return throwError(e);
+      })
+    );
   }
 
   update(cliente: Cliente): Observable<Cliente> {
